ba997f63fd7c90b6b73a7f0df91b5e3db0969a5f
Mio <s89652158910@gmail.com>
2023-06-30 12:31:19 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Hors;
+using System;
+
+var hors = new HorsTextParser();
+string text_raw = $"31 декабря {DateTime.Now.ToString("yyyy")} год";
+var result = hors.Parse(text_raw, DateTime.Now);
+
+var text = result.Text; 
+var formatText = result.TextWithTokens; 
+var date = result.Dates[0].DateFrom; 
+System.Console.WriteLine(date);
+System.Console.WriteLine(DateTime.Now);
