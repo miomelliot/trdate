78b27336327ab076a458b214308041575812ef53
Mio <s89652158910@gmail.com>
2023-06-30 15:57:47 +0300

    _

--- Transform/Transform.py
+++ Transform/Transform.py
@@ -1,15 +1,18 @@
 import re 
+import datetime
 
 # YYYY-MM-DD HH24:MI:SS
 class Transform() :
     def __init__(self) :
-        self.year = ""
-        self.month = ""
-        self.day = ""
-        self.hour = ""
-        self.minute = ""
+        self.year = datetime.datetime.now().year
+        self.month = datetime.datetime.now().month
+        self.day = datetime.datetime.now().day
+        self.hour = "00"
+        self.minute = "00"
+        self.second = "00"
         
     def splitLines(self, lines):
+        lines = re.sub(r":", " ", lines)
         lines = lines.split()
         return lines
     
@@ -35,8 +38,8 @@ class Transform() :
                 list_array[i] = f"{dict_month[item]}MM"
                 self.month = dict_month[item]
                 if i != 0:    
-                    list_array[i-1] = f"{list_array[i-1]}DD"
                     self.day = list_array[i-1]
+                    list_array[i-1] = f"{list_array[i-1]}DD"
         return list_array
         
         
@@ -46,12 +49,31 @@ class Transform() :
             if re.search(r"^час$", item):
                 list_array[i] = "1HH"
                 self.hour = list_array[i]
-            elif re.search(r"час", item) and re.search(r"[0-9]", list_array[i - 1], re.IGNORECASE) :
-                
-                print(1)
-        return list_array
-                
-                
+                return list_array
+            elif re.search(r"час", item) and re.search(r"[0-9]", list_array[i - 1], re.IGNORECASE) and not re.search(r"дня|вечер|ноч", list_array[i + 1]):
+                if int(list_array[i - 1]) < 24:
+                    self.hour = list_array[i-1]
+                    list_array[i-1] = f"{list_array[i-1]}HH"
+                    return list_array
+            elif re.search(r"дня|вечер|ноч", list_array[i + 1]):
+                if int(list_array[i - 1]) in (9, 10, 11):
+                    list_array[i - 1] = int(list_array[i - 1]) + 12
+                elif int(list_array[i - 1]) == 12:
+                    list_array[i - 1] = int(list_array[i - 1]) - 12
+                self.hour = list_array[i-1]
+                list_array[i-1] = f"{list_array[i-1]}HH"
+                return list_array
+        
+    
+    def get_date_list(self, list) :
+        self.monthLinesTime(list)
+        self.hourLines(list)
+        date_string = f"{self.year}-{self.month}-{self.day} {self.hour}:{self.minute}:{self.second}"
+        date_format = "%Y-%m-%d %H:%M:%S"
+        print(datetime.datetime.strptime(date_string, date_format))
+    
+    
+    
 if __name__ == '__main__':
-    transform = Transform()
-    print(transform.monthLinesTime("5 мая в 7 часов утра"))
+    t = Transform()
+    t.get_date_list("Вчера в 13:44")
