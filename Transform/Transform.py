703d5e5fa2adf5dbd205e824feb3175b042bb295
Mio <s89652158910@gmail.com>
2023-07-07 09:42:54 +0300

    Обновлено

--- Transform/Transform.py
+++ Transform/Transform.py
@@ -1,9 +1,8 @@
 import re
 import datetime as dt
+from datetime import timedelta
 
 # YYYY-MM-DD HH24:MI:SS
-
-
 class Transform():
     def __init__(self):
         self.year = dt.datetime.now().year
@@ -12,6 +11,7 @@ class Transform():
         self.hour = "00"
         self.minute = "00"
         self.second = "00"
+        self.list_array = None
 
     # .join
     def stringLinse(self, string):
@@ -25,20 +25,22 @@ class Transform():
         return lines
 
     # YYYY format
-    def yearLines(self, lines):
-        list_array = self.splitLines(lines)
-        for i, item in enumerate(list_array):
+    def yearLines(self):
+        for i, item in enumerate(self.list_array):
             if re.search(r"^год$", item):
-                self.year = f"20{list_array[i-1]}" if len(
-                    f"20{list_array[i-1]}") == 4 else dt.datetime.now().year
-                if len(f"{list_array[i-1]}") == 4 and str(list_array[i-1])[:2] == "20":
-                    self.year = f"{list_array[i-1]}" if len(
-                        f"{list_array[i-1]}") == 4 else dt.datetime.now().year
-                list_array[i] = f"{self.year}YY"
-                return self.stringLinse(list_array)
-
+                self.year = f"20{self.list_array[i-1]}" if len(
+                    f"20{self.list_array[i-1]}") == 4 else dt.datetime.now().year
+                if len(f"{self.list_array[i-1]}") == 4 and str(self.list_array[i-1])[:2] == "20":
+                    self.year = f"{self.list_array[i-1]}" if len(
+                        f"{self.list_array[i-1]}") == 4 else dt.datetime.now().year
+                self.list_array[i] = f"{self.year}YY"
+                return self.list_array
+            elif re.search(r"^20[0-9]{2}$", item, re.IGNORECASE):
+                self.list_array[i] = f"{self.year}YY"
+                return self.list_array
+            
     # MM and DD formats
-    def monthLinesTime(self, list_raw):
+    def monthLinesTime(self):
         dict_month = {
             "января": "1",
             "февраля": "2",
@@ -53,43 +55,74 @@ class Transform():
             "ноября": "11",
             "декабря": "12"
         }
-        list_array = self.splitLines(list_raw)
-        for i, item in enumerate(list_array):
+        for i, item in enumerate(self.list_array):
             if item in dict_month:
-                list_array[i] = f"{dict_month[item]}MM"
+                self.list_array[i] = f"{dict_month[item]}MM"
                 self.month = dict_month[item]
                 if i != 0:
-                    self.day = list_array[i-1]
-                    list_array[i-1] = f"{list_array[i-1]}DD"
-        return self.stringLinse(list_array)
+                    self.day = self.list_array[i-1]
+                    self.list_array[i-1] = f"{self.list_array[i-1]}DD"
+                    return self.stringLinse(self.list_array)
+            elif re.search(r"вчера", item, re.IGNORECASE):
+                yesterday = dt.datetime.now() - dt.timedelta(days=1)
+                month_date = yesterday.strftime("%m")
+                day_date = yesterday.strftime("%d")
+                self.list_array[i] = f"{month_date}MM {day_date}DD"
+                self.month = month_date
+                self.day = day_date
+                return self.list_array
+            elif re.search(r"позавчера", item, re.IGNORECASE):
+                day_before_yesterday = dt.datetime.now() - dt.timedelta(days=2)
+                month_date = day_before_yesterday.strftime("%m")
+                day_date = day_before_yesterday.strftime("%d")
+                self.list_array[i] = f"{month_date}MM {day_date}DD"
+                self.month = month_date
+                self.day = day_date
+                return self.list_array
+        return self.list_array
 
     # HH format
-    def hourLines(self, list_raw):
-        list_array = self.splitLines(list_raw)
-        for i, item in enumerate(list_array):
+    def hourLines(self):
+        for i, item in enumerate(self.list_array):
             if re.search(r"^час$", item):
-                list_array[i] = "1HH"
-                self.hour = list_array[i]
-                return self.stringLinse(list_array)
-            elif re.search(r"час", item) and re.search(r"[0-9]", list_array[i - 1], re.IGNORECASE) and not re.search(r"дня|вечер|ноч", list_array[i + 1]):
-                if int(list_array[i - 1]) < 24:
-                    self.hour = list_array[i-1]
-                    list_array[i-1] = f"{list_array[i-1]}HH"
-                    return self.stringLinse(list_array)
-            elif list_array[-1] != item and re.search(r"дня|вечер|ноч", list_array[i + 1]):
+                self.hour = self.list_array[i]
+                self.list_array[i] = "1HH"
+                return self.list_array
+            elif re.search(r"час", item) and re.search(r"[0-9]", self.list_array[i - 1], re.IGNORECASE) and not re.search(r"дня|вечер|ноч", self.list_array[i + 1]):
+                if int(self.list_array[i - 1]) < 24:
+                    self.hour = self.list_array[i-1]
+                    self.list_array[i-1] = f"{self.list_array[i-1]}HH"
+                    return self.list_array
+            elif re.search(r"^полдень$", item):
+                self.hour = "12"
+                self.list_array[i] = "12HH"
+                return self.list_array
+            elif self.list_array[-1] != item and re.search(r"дня|вечер|ноч", self.list_array[i + 1]):
                 try:
-                    if int(list_array[i - 1]) in (9, 10, 11):
-                        list_array[i - 1] = int(list_array[i - 1]) + 12
-                    elif int(list_array[i - 1]) == 12:
-                        list_array[i - 1] = int(list_array[i - 1]) - 12
+                    if int(self.list_array[i - 1]) in (9, 10, 11):
+                        self.list_array[i - 1] = int(self.list_array[i - 1]) + 12
+                    elif int(self.list_array[i - 1]) == 12:
+                        self.list_array[i - 1] = int(self.list_array[i - 1]) - 12
+                    elif re.search(r"дня|вечер", self.list_array[i + 1]) and int(self.list_array[i - 1]) < 12:
+                        self.list_array[i - 1] = int(self.list_array[i - 1]) + 12
                 except ValueError:
-                    if int(list_array[i]) in (9, 10, 11):
-                        list_array[i] = int(list_array[i]) + 12
-                    elif int(list_array[i]) == 12:
-                        list_array[i] = int(list_array[i]) - 12
-                self.hour = list_array[i]
-                list_array[i-1] = f"{list_array[i]}HH"
-                return self.stringLinse(list_array)
+                    if int(self.list_array[i]) in (9, 10, 11):
+                        self.list_array[i] = int(self.list_array[i]) + 12
+                    elif int(self.list_array[i]) == 12:
+                        self.list_array[i] = int(self.list_array[i]) - 12
+                    elif re.search(r"дня|вечер", self.list_array[i]):
+                        self.list_array[i] = int(self.list_array[i]) + 12
+                self.hour = self.list_array[i-1]
+                self.list_array[i-1] = f"{self.list_array[i-1]}HH"
+                return self.list_array
+            elif re.search(r"^(0?[1-9]|1[0-9]|2[0-3])$", item, re.IGNORECASE) and len(self.list_array) == i+2:
+                self.hour = self.list_array[i]
+                self.list_array[i] = f"{self.list_array[i]}HH"
+                if re.search(r"^(0?[1-9]|[1-5][0-9]|59)$", self.list_array[i+1], re.IGNORECASE):
+                    self.minute = self.list_array[i+1]
+                    self.list_array[i+1] = f"{self.list_array[i]}MI"
+                return self.list_array
+                    
             # elif re.search(r"пол", item, re.IGNORECASE):
             #     if len(list_array) >= i + 1:
             #         if int(list_array[i + 1]) in (9, 10, 11):
@@ -99,28 +132,30 @@ class Transform():
             #         self.hour = list_array[i+1]
             #         list_array[i+1] = f"{list_array[i+1]}HH"
 
-    
-    def get_date_list(self, list, flags=None):
-        self.yearLines(list)
-        self.monthLinesTime(list)
-        self.hourLines(list)
+    def get_d(self):
         date_string = f"{self.year}-{self.month}-{self.day} {self.hour}:{self.minute}:{self.second}"
         date_format = "%Y-%m-%d %H:%M:%S"
         parsed_date = dt.datetime.strptime(date_string, date_format)
+        return parsed_date
+
+    def get_date_list(self, line):
+        self.list_array = self.splitLines(line)
+        a = self.yearLines()
+        b = self.monthLinesTime()
+        c = self.hourLines()
+        
+        parsed_date = self.get_d()
 
         # Получение текущей даты
         now = dt.datetime.now()
 
-        if flags is not None:
-            if "p" in flags and parsed_date > now:
-                parsed_date = now
-            elif "r" in flags and parsed_date.date() > now.date():
-                parsed_date = now
-        elif parsed_date > now:
-            print("Ошибка: указанная дата не может быть больше текущей даты без установленного флага.")
-            return
+        if parsed_date > now:
+            self.year = int(now.year) - 2
+            parsed_date = self.get_d()
+            print(parsed_date)
+        else:
+            print(parsed_date)
 
 
 if __name__ == '__main__':
-    t = Transform()
-    t.get_date_list("5 мая 2024 год 17 51")
+    Transform().get_date_list("18:03 16 числа 0 5 месяца 2 1000 23 года")
